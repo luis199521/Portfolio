6116de967ad9c23182e5a087e5cf8b2a2a5cb7f9
Luis Alvarez <34248833+luis199521@users.noreply.github.com>
2025-08-30 11:40:57 +0800

    fix(component)
    set correct name for Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Navbar from './components/Navbar';
-import Home from './components/home';
+import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
 import Education from './components/Education';
